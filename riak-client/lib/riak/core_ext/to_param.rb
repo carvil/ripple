fb093f9cf7e3711db2548a0468f8072089078ec9
carvil <c.vilhena@digital-science.comm>
2011-10-12 16:50:03 +0100

    using riak escaping instead of enforcing cgi escaping

--- riak-client/lib/riak/core_ext/to_param.rb
+++ riak-client/lib/riak/core_ext/to_param.rb
@@ -5,8 +5,8 @@ unless Object.new.respond_to? :to_query and Object.new.respond_to? :to_param
     end
 
     def to_query(key)
-      require 'cgi' unless defined?(CGI) && defined?(CGI::escape)
-      "#{CGI.escape(key.to_s)}=#{CGI.escape(to_param.to_s)}"
+      include Riak::Util::Escape
+      "#{escape(key.to_s)}=#{escape(to_param.to_s)}"
     end
   end
 
